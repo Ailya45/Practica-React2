e21fe24d10d72157f35f47d8f2d7d28863ba6ebe
Ailya45 <bermysjose999@gmail.com>
2025-10-31 22:58:12 -0400

    change a vercel

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
-  server: {
-    host: true,
-    strictPort: true,
-    port: process.env.PORT || 3000,
-
-    hmr: {
-      port: process.env.PORT 
-    },
-
-    host: '0.0.0.0', 
-    allowedHosts: [
-      'practica-react2-1.onrender.com',
-    ]
-
-  }
 })
 

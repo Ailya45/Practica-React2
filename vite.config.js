941109b75c721a2212054bed1f9050aa22b7d6a4
Ailya45 <bermysjose999@gmail.com>
2025-10-31 21:41:00 -0400

    update host

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: true,
     strictPort: true,
     port: process.env.PORT || 3000
   }
